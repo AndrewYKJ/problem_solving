1d990e007cc6ce7b7358837e458d0d9e6e7a4f21
Andrew YapKJ <yapkj96@gmail.com>
2022-05-10 15:09:20 +0800

    Update 34_Find_First_and_Last_Position_of_Element_in_Sorted_Array.js

--- js/34_Find_First_and_Last_Position_of_Element_in_Sorted_Array.js
+++ js/34_Find_First_and_Last_Position_of_Element_in_Sorted_Array.js
@@ -1,1 +1,29 @@
-
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var searchRange = function(nums, target) {
+    if(nums.length == 0) return [-1,-1];
+    
+    let len = nums.length - 1
+    let start = 0;
+    let first = 0;
+    let last = 0;
+    for(let x = 0; x <= len; x++){
+        if(nums[x]===target){
+        first = x
+        break;
+        }
+        first = -1;
+    }
+    for(let y = len; y >= 0; y--){
+        if(nums[y]===target){
+        last = y
+        break;
+        }
+        last = -1;
+    }
+    
+    return [first,last]
+};
